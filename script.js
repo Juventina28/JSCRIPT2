0a0e34c061725fc376b66a2279225d66d3278f61
Juventina28 <juventinabinga@gmail.com>
2025-07-29 17:07:06 +0100

    listagem com materialize

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function adicionarTarefa(){ 
-    let mensagem = "Tarefa adicionada com sucesso"
-    let inputTarefa =document.getElementById("inputTarefa")
-    let tarefa = inputTarefa.value
+ let tarefas=[]
+ 
+ function adicionarTarefa(){ 
 
+    
+    /*
     if(tarefa==""){
-        alert("Por favor, isira uma tarefa válida.");
-
+        alert("Por favor, insira uma tarefa válida.");
         return;
     }else{
         document.getElementById("mensagem").textContent = mensagem;
@@ -18,6 +18,31 @@ function adicionarTarefa(){
         listaTarefas.appendChild(novaTarefa)
 
         inputTarefa.value=""
-    }
+    }*/
+    const inputTarefa =document.getElementById("inputTarefa")
+    let tarefa = inputTarefa.value.trim()
+    const mensagem = document.getElementById("mensagem");
 
+    if(tarefa==""){
+        let mensagemErro= "Digite uma tarefa para adicioná-la"
+        mensagem.textContent=mensagemErro
+    } else{
+        let mensagemSucesso ="Tarefa adicionada com sucesso!"
+        mensagem.textContent = mensagemSucesso
+
+        tarefas.push(tarefa)
+        renderizarTarefas()
+     }
+    inputTarefa.value=""
+}
+function renderizarTarefas(){
+            const listaTarefas = document.getElementById("listaTarefas")
+            listaTarefas.innerHTML=""
+
+            let i=0
+            for(i; i<tarefas.length; i++){
+                let novaTarefa=document.createElement("li")
+                novaTarefa.textContent=tarefas[i]
+                listaTarefas.appendChild(novaTarefa)
+            }
 }
